d63ed0a3109effa7229be2c449dfcde354f09f77
Ivan Boldyrev <lispnik@gmail.com>
2013-06-02 17:24:56 +0700

    List posts in with categories.
    Lot of refactoring involved.

--- src/terablogger/core.clj
+++ src/terablogger/core.clj
@@ -33,20 +33,38 @@
   "Blog's base dir."
   (:blog-dir *cfg*))
 
+(def ^:dynamic *data-dir*
+  "Blog's data dir."
+  (str *blog-dir* File/separator "data"))
+
 (defmacro with-config
-  "Bind *cfg* and *blog-dir* to values if cfg and execute body."
+  "Bind *cfg*, *blog-dir* and *data-dir* to values in cfg and execute body."
   [cfg & body]
   `(binding [*cfg* ~cfg]
      (binding [*blog-dir* (:blog-dir *cfg*)]
-       ~@body)))
+       (binding [*data-dir* (str *blog-dir* File/separator "data")]
+         ~@body))))
 
 (defn blog-path
-  [path]
-  (str *blog-dir* path))
+  "Path for blog file."
+  [filename]
+  (str *blog-dir* File/separator filename))
 
 (defn blog-file
-  [path]
-  (File. (blog-path path)))
+  "java.io.File for blog file."
+  [filename]
+  (File. (blog-path filename)))
+
+
+(defn data-path
+  "Path for data file."
+  [filename]
+  (str *data-dir* File/separator filename))
+
+(defn data-file
+  "java.io.File for data file."
+  [filename]
+  (File. (data-path filename)))
 
 
 (defn data-lister
@@ -55,9 +73,12 @@
   (fn []
     (let [dir (blog-file "/data")]
       (sort cmp
-            (filter (comp (partial re-seq regex) 
-                          #(.getName %))
-                    (file-seq dir))))))
+            (filter (partial re-seq regex)
+                    (map
+                     ;; Last component of path
+                     #(last (string/split (.getName %)
+                                          (re-pattern File/separator)))
+                     (file-seq dir)))))))
 
 (def list-cats
   "Function that return list of category files."
@@ -76,36 +97,59 @@
 ;;;
 ;;; Categories
 ;;;
-(defn parse-cat [file]
-  (let [txt (slurp file)
+(defn parse-cat
+  "Parse category file."
+  [file]
+  (let [txt (slurp (data-file file))
         [name & files] (string/split-lines txt)]
     {:name name
-     :files files}))
+     :files files
+     :set (set files)}))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;
 ;;; Posts
 ;;;
 
-(defn parse-headers [headers]
-  (apply hash-map
-         (mapcat #(let [[key val] (string/split % #": " 2)]
-                    (list (keyword key) val))
+(defn parse-headers
+  "Parse post's headers, returning a hash."
+  [headers]
+  (into {}
+         (map #(let [[key val] (string/split % #": " 2)]
+                    [(keyword key) val])
                  headers)))
 
-(defn parse-post [file]
-  (let [txt (slurp file)
+(defn parse-post 
+  "Parse blog post."
+  [file]
+  (let [txt (slurp (data-file file))
         lines (string/split-lines txt)
         [headers body] (split-with #(not (re-seq #"^-----$" %)) lines)]
-    [(parse-headers headers) (string/join "\n" (butlast (rest (rest body))))]))
+    [(parse-headers headers)
+     (string/join "\n" (butlast (rest (rest body))))
+     file]))
 
-(defn ls-posts [posts]
-  (dorun
-   (for [[[p b] n] (map list posts (range))]
-     (println (format "%d. %s - %s"
-                      (inc n)
-                      (:TITLE p)
-                      (:DATE p))))))
+(defn posts-cats
+  "Return list of cats post belongs to."
+  [post cats]
+  (let [[_ _ f] post]
+   (filter #((:set %) f) cats)))
+
+(defn ls-posts
+  ([posts]
+     (ls-posts posts (map parse-cat (list-cats))))
+  ([posts cats]
+     (dorun
+      (for [[post n] (map list posts (range))]
+        (let [[p _ _] post
+              pcats (posts-cats post cats)]
+          (print (format "%d. %s"
+                           (inc n)
+                           (:TITLE p)))
+          (when (seq pcats)
+            (print (format " - [%s]" (string/join ", " (map :name pcats)))))
+          (println (format " - %s" (:DATE p))))))))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
