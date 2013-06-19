e9abbcfe70a07fce694e375b6411d48ec0d189f6
Ivan Boldyrev <lispnik@gmail.com>
2013-06-19 15:21:10 +0700

    Comment apath module.

--- src/terablogger/apath.clj
+++ src/terablogger/apath.clj
@@ -3,32 +3,47 @@
   (:require [terablogger.cfg :as cfg]
             [clojure.string :as string]))
 
+;;;
+;;; Apath is a vector of strings that represents path relative to both
+;;; blog dir and blog url.
+;;;
+;;; File pathes and URL may have different separators, so we delay
+;;; converting path to string until last moment.
+;;;
+
 (defn url-path
+  "Just relative path (withoug (*cfg* :url) or something like this)."
   [apath]
   (string/join "/" apath))
 
 (defn full-url-path
+  "Convert apath to full URL (with (*cfg* :url))."
   [apath]
   [str (:url cfg/*cfg*) (url-path apath)])
 
 (defn blog-path
-  "Path for blog file."
+  "Path for file in blog dir."
   [apath]
   (string/join File/separator (cons cfg/*blog-dir* apath)))
 
 (defn data-path
-  "Path for data file."
+  "Path for file in data dir.  Note that it accepts just filename, as 
+data dir has no nested folders."
   [filename]
   (string/join  File/separator (list cfg/*blog-dir* "data" filename)))
 
-(defn archive [apath]
+(defn archive
+  "Construct apath within archive."
+  [apath]
   (into ["archive"] apath))
 
-(defn cache [apath]
+(defn cache
+  "Construct apath within a cache."
+  [apath]
   (into ["parts"] apath))
 
 (defn data-lister
-  "List files in data dir that match regex"
+  "List files in data dir that match regex."
   [regex cmp]
   (fn []
     (let [dir (blog-path ["data"])]
