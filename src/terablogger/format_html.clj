cb0cb4b8ff6b1cb4a4cf20841f38412defb65702
Ivan Boldyrev <lispnik@gmail.com>
2013-06-23 21:31:38 +0700

    Implement auto-break in HTML.

--- src/terablogger/format_html.clj
+++ src/terablogger/format_html.clj
@@ -2,9 +2,7 @@
   (:require [clojure.string :as string]))
 
 (defn auto-break [txt]
-  ; STUB
-  txt
-  )
+  (string/replace txt #"(\r\r|\n\n|\r\n\rn)" "<br>\n"))
 
 (defn html-escape [txt]
   (string/escape txt
